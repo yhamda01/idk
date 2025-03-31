3b27b2997e47269761f4c919cfe64a513ebb8663
yhamda01 <126518907+yhamda01@users.noreply.github.com>
2025-03-31 18:09:45 -0400

    Create try.js

---
+++ try.js
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <script type="text/javascript">
+        window.location.href = 'https://raw.githubusercontent.com/yhamda01/idk/main/scriptt.js';
+    </script>
+</head>
+<body>
+    <p>If not redirected, <a href="https://raw.githubusercontent.com/yhamda01/idk/main/scriptt.js">click here</a>.</p>
+</body>
+</html>
